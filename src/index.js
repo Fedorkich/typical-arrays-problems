9152be87ef188b95fbd4f249ac9e3b500454aeb3
Fedorkich <kichfed20@gmail.com>
2021-03-13 15:47:21 +0300

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if( typeof array!="undefined" && array!= null && array.length>0) {
+  let minimum=array[0];
+  for(let i=1;i<array.length;i++){
+    if(array[i]<minimum) minimum=array[i]
+  }
+  return minimum;
+}
+return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if( typeof array!="undefined" && array!= null && array.length>0) 
+  {
+  let maximum=array[0]
+  for(let i=1;i<array.length;i++){
+    if(array[i]>maximum) maximum=array[i]
+  }
+  return maximum;
+}
+return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array!="undefined" && array!= null && array.length>0) 
+  {
+  let sum=0, average=0;
+  for(let i=0;i<array.length;i++){
+    sum+=array[i];
+     }
+  average=sum/array.length;
+  return average;
+}
+return 0;
 }
